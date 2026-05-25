0e55e741efffe3bdacb64ad34256d3b95dd191d0
Nirmal Kharche <nirmalkharche2006@gmail.com>
2026-05-26 00:52:31 +0530

    Add files via upload

---
+++ bitwise operators and increment decre/1bitwise.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=10;
+    int b=1;
+    cout<<"a&b :" <<(a&b) <<endl;  // bitwise and
+     cout<<"a|b :" <<(a|b) <<endl; // bitwise or
+     cout<<"~a :" <<~a <<endl;  // bitwise not
+     cout<<"a^b :" <<(a^b)<<endl;// bitwise xor
+     cout<<"a<<b: "<<(a<<b)<<endl;// bitwise left shift
+     cout<<"a>>b: "<< (a>>b)<<endl;// bitwise right shift
+     
+}
